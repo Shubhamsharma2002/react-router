8ff1843a4aa32c526731345c06f6159fd4e331c8
shubham sharma <shubhamtech004@gmail.com>
2024-09-10 17:12:36 +0530

    setup the rotes using react router dom

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,37 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
 
+import './index.css'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Layout from './Layout.jsx'
+import Home from './component/home/home.jsx'
+import About from './component/About/about.jsx'
+import Contact from './component/Contact/contact.jsx'
+
+
+const router = createBrowserRouter([
+  {
+    path:'/',
+    element:<Layout />,
+    children:[
+      {
+        path:"",
+        element:<Home />
+      },
+      {
+        path:"about",
+        element:<About />
+      },
+      {
+        path:"contact",
+        element:<Contact />
+      },
+      
+    ]
+  }
+])
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router}/>
   </StrictMode>,
 )
