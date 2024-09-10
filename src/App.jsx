cfa26b7cfc5db2177f29bb3fe8d06d7cd4550814
shubham sharma <shubhamtech004@gmail.com>
2024-09-10 16:19:20 +0530

    project setup done for router based project

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+   <>
+   <h1 className='text-green-500 text-center '>hlw router </h1>
+   
+   </>
+  )
+}
+
+export default App
